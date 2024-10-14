30de1b9c27bfedbe7fe07381d488ce43acbd5971
hamada-minoro <hamada.minoro.0@gmail.com>
2024-10-14 12:40:38 -0300

    24:20 Aula-03

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -2,11 +2,25 @@
 
 namespace App\Livewire\Proposals;
 
+use App\Models\Project;
 use Livewire\Component;
 
 class Create extends Component
 {
+    public Project $project;
     public bool $modal = false;
+    public string $email = ' ';
+    public int $hours = 0;
+
+    public function save(){
+
+        $this->project->proposals()
+        ->updateOrCreate(
+            ['email' => $this->email],
+            ['hours'=> $this->hours]
+        );
+
+    }
 
     public function render()
     {
